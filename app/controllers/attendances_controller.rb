86a7a128c2e95bacc58408c94a2046da7f51965b
takayuki-iguchi <taka21cent@yahoo.co.jp>
2020-02-07 22:40:03 +0000

    勤怠Ｂ

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,15 +1,17 @@
 class AttendancesController < ApplicationController
+  include AttendancesHelper
   before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :logged_in_user, only: [:update, :edit_one_month]
+  before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
   before_action :set_one_month, only: :edit_one_month
-
+  
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
 
   def update
     @user = User.find(params[:user_id])
     @attendance = Attendance.find(params[:id])
     # 出勤時間が未登録であることを判定します。
-    if @attendance.started_at.nil?
+    if @attendance.started_at.nil? 
       if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
         flash[:info] = "おはようございます！"
       else
@@ -29,14 +31,21 @@ class AttendancesController < ApplicationController
   end
 
   def update_one_month
-   ActiveRecord::Base.transaction do # トランザクションを開始します。
+   ActiveRecord::Base.transaction do# トランザクションを開始します。
+    if attendances_invalid?
+        
+   
      attendances_params.each do |id, item|
       attendance = Attendance.find(id)
       attendance.update_attributes!(item)
-     end
-   end
-   flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
-   redirect_to user_url(date: params[:date])
+     end 
+     flash[:success] = "1ヶ月分の勤怠情報を更新しました。"
+     redirect_to user_url(date: params[:date])
+    else
+     flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+        redirect_to attendances_edit_one_month_user_url(date: params[:date])
+    end
+    end
   rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
    flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
    
@@ -54,7 +63,7 @@ class AttendancesController < ApplicationController
 
     # 管理権限者、または現在ログインしているユーザーを許可します
     def admin_or_correct_user
-      @user = User.fnd(params[:user_id]) if @user.blank?
+      @user = User.find(params[:user_id]) if @user.blank?
       unless current_user?(@user) || current_user.admin?
         flash[:danger] = "編集権限がありません。"
         redirect_to(root_url)
@@ -63,3 +72,4 @@ class AttendancesController < ApplicationController
   
   
 end
+
