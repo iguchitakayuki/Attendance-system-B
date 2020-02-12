8f22b1b89ee3bb26968a1816cd1525bfee1aa43f
takayuki-iguchi <taka21cent@yahoo.co.jp>
2020-02-12 11:30:20 +0000

    Finish 勤怠B-8:28

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,14 @@
 # coding: utf-8
 
 User.create!(name: "Sample User",
-             email: "sample-user@email.com",
+             email: "sampleuser@email.com",
              password: "password",
              password_confirmation: "password",
              admin: true)
              
 60.times do |n|
   name = Faker::Name.name
-  email = "sample:#{n+1}@email.com"
+  email = "sample::#{n+1}@email.com"
   password = "password"
   User.create!(name: name,
                email: email,
